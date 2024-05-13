0b6cd138bf2cd28a06af9377313b6a80ec6b7f96
Elena <lenvigo2308@gmail.com>
2024-05-13 08:55:24 +0200

    feat <VeterinarioPrincipal>: code menu operational

--- src/main/java/programacion/ClinicaInterface.java
+++ src/main/java/programacion/ClinicaInterface.java
@@ -1,14 +1,21 @@
-public interface ClinicaInterface {
-    void insertVet();
-    void insertMasc();
-    void deleteVet(String dni);
-    void deleteMasc(int id);
-    void showPetData();
-    void showVetFromMasc();
+package programacion;
+
+import org.mariadb.jdbc.Statement;
+
+class Clinica{
+    public static void insertVet(Statement sentencia){};
+    public static void insertMasc(Statement sentencia){}
+    public static void deleteVet(Statement sentencia){}
+    public static void deleteMasc(Statement sentencia){}
+    public static void showPetData(Statement sentencia){}
+    public static void showVetFromMasc(Statement sentencia){}
 
     // Se debe devolver la ruta al fichero exportado
-    String exportVetHighSalary();
-    void modifyVetSalary();
-    void modifyVetMasc();
+    public static String exportVetHighSalary(Statement sentencia){
+
+        return null;
+    }
+    public static void modifyVetSalary(Statement sentencia){}
+    public static void modifyVetMasc(Statement sentencia){}
 }
 
--- src/main/java/programacion/VeterinarioPrincipal.java
+++ src/main/java/programacion/VeterinarioPrincipal.java
@@ -11,6 +11,7 @@ public class VeterinarioPrincipal {
 
 
     public static void main(String[] args) {
+
         Statement sentencia = null;
         Connection conn = null;
 
@@ -19,20 +20,115 @@ public class VeterinarioPrincipal {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         try {
-         conn= (Connection) DriverManager.getConnection("jdbc:mariadb://localhost:3306/?user=root&password=");
-        }catch(SQLException e){
+            conn = (Connection) DriverManager.getConnection("jdbc:mariadb://localhost:3306/?user=root&password=");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        sentencia=conn.createStatement();
 
+        sentencia = conn.createStatement();
+
+        Creacion.crearBase(sentencia);
 
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
+        int op1 = 0;
+        int op2 = 0;
+        int op3 = 0;
+        int op4 = 0;
+        boolean salir = false;
 
         do {
             Menus.showOptions();
             opcion = sc.nextInt();
+            switch (opcion) {
+
+                case 1:
+                    while (salir == false) {
+                        Menus.showOptionsInsert();
+                        op1 = sc.nextInt();
+                        switch (op1) {
+                            case 1: // Insertar mascota
+                                Clinica.insertMasc(sentencia);
+                                break;
+                            case 2://Insertar veterinario
+                                Clinica.insertVet(sentencia);
+                                break;
+                            case 3:
+                                salir = true;
+                                break;
+
+                        }
+
+                    }
+                    salir = false;
+                    break;
+                case 2:
+                    while (salir == false) {
+                        Menus.showOptionsDelete();
+                        op2 = sc.nextInt();
+                        switch (op2) {
+                            case 1: // Eliminar mascota
+                                Clinica.deleteMasc(sentencia);
+                                break;
+                            case 2: // Eliminar veterinario
+                                Clinica.deleteVet(sentencia);
+                                break;
+                            case 3:
+                                salir = true;
+                                break;
+
+                        }
+
+                    }
+                    salir = false;
+                    break;
+                case 3:
+                    while (salir == false) {
+                        Menus.showOptionsQueries();
+                        op3 = sc.nextInt();
+                        switch (op3) {
+                            case 1: //Mostrar mascotas de un veterinario
+                                Clinica.showPetData(sentencia);
+                                break;
+                            case 2: //Mostrar veterinario de una mascota
+                                Clinica.showVetFromMasc(sentencia);
+                                break;
+                            case 3: //Exportar a fichero los datos de veterinarios con salario alto
+                               Clinica.exportVetHighSalary(sentencia);
+                                break;
+                            case 4:
+                                salir = true;
+                                break;
+
+                        }
+
+                    }
+                    salir = false;
+                    break;
+                case 4:
+                    while (salir == false) {
+                        Menus.showOptionsModify();
+                        op4 = sc.nextInt();
+                        switch (op4) {
+                            case 1: // Modificar salario de veterinario
+                                Clinica.modifyVetSalary(sentencia);
+                                break;
+                            case 2: //Modificar veterinario asignado a mascota
+                                Clinica.modifyVetMasc(sentencia);
+                                break;
+                            case 3:
+                                salir = true;
+                                break;
+
+                        }
+
+                    }
+                    salir = false;
+                    break;
+
+            }
 
 
         } while (opcion != 5);
