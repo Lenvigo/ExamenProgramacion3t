8171f8b015b0e69b9372648b8d72765ea2ab9751
Elena <lenvigo2308@gmail.com>
2024-05-13 08:27:09 +0200

    conexion a la bd

--- src/main/java/programacion/VeterinarioPrincipal.java
+++ src/main/java/programacion/VeterinarioPrincipal.java
@@ -1,10 +1,32 @@
 package programacion;
+
+import org.mariadb.jdbc.Connection;
+import org.mariadb.jdbc.Statement;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class VeterinarioPrincipal {
 
 
     public static void main(String[] args) {
+        Statement sentencia = null;
+        Connection conn = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+         conn= (Connection) DriverManager.getConnection("jdbc:mariadb://localhost:3306/?user=root&password=");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        sentencia=conn.createStatement();
+
+
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
 
